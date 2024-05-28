b5e554d5f6f682f80441c62c32edf05d54091209
NICOLLAS JOSE PRIM <nicollas_prim@estudante.sesisenai.org.br>
2024-05-28 12:26:06 -0300

    first commit

---
+++ prog01.js
@@ -0,0 +1,16 @@
+let resposta = document.getElementById('resposta')
+
+function calcular(){
+    let lado = Number(document.getElementById('lado').value)
+    console.log("O valor digitado é = ",lado)
+
+    let volumeCubo = lado * lado * lado
+    console.log('O volume do cubo é = ', volumeCubo)
+
+    resposta.innerHTML = "O volume do cubo é = " + volumeCubo +
+                                        " m3"
+
+    resposta.style.fontSize = "2rem"
+    resposta.style.fontWeight = "bold"
+    resposta.style.color = "yellow"
+}
